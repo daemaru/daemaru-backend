3a6e0e7dbc27867b8a08e88f48758aab9df6a578
velyn666 <ni1722650@gmail.com>
2024-12-18 11:42:46 +0900

    chore :: getKey, from 어노테이션 삭제, 롬복 사용

--- src/main/java/com/demaru/domain/user/domain/roleEnum/Role.java
+++ src/main/java/com/demaru/domain/user/domain/roleEnum/Role.java
@@ -1,23 +1,13 @@
 package com.demaru.domain.user.domain.roleEnum;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum Role {
     TEACHER("교사"),
     STUDENT("학생");
 
-    @Getter
     private final String description;
-
-    Role(String description) {
-        this.description = description;
-    }
-
-    public static Role from(String role) {
-        return Role.valueOf(role.toUpperCase());
-    }
-
-    public String getKey() {
-        return name();
-    }
 }
