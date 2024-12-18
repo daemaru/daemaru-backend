c9da69912ebd3f7ad9c8d11e946070c829b0680e
khyunwook <khyunwook0707@gmail.com>
2024-12-18 09:22:37 +0900

    initial commit

---
+++ src/main/java/com/demaru/DemaruApplication.java
@@ -0,0 +1,13 @@
+package com.demaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemaruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemaruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demaru/DemaruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemaruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
