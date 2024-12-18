fefac20e24127604fcb8b4c7fe22856dc4f9a42e
velyn666 <ni1722650@gmail.com>
2024-12-18 10:41:57 +0900

    test :: aaa

--- src/main/java/com/demaru/DemaruApplication.java
+++ src/main/java/com/demaru/DemaruApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemaruApplication {
 
+
+    
     public static void main(String[] args) {
         SpringApplication.run(DemaruApplication.class, args);
     }
