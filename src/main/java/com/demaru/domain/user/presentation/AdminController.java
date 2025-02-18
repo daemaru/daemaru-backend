b1b771eac69bcbffd23c18688530f766bfc4fdea
velyn666 <ni1722650@gmail.com>
2025-02-18 20:01:10 +0900

    fix :: 코멘트 반영

--- src/main/java/com/demaru/domain/user/presentation/AdminController.java
+++ src/main/java/com/demaru/domain/user/presentation/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admins")
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
