b7c953e884c883dbfdb2efdae232912b1f091ff5
velyn666 <ni1722650@gmail.com>
2025-02-24 19:54:25 +0900

    chore :: Refresh Token 삭제

--- src/main/java/com/demaru/global/security/jwt/JwtProvider.java
+++ src/main/java/com/demaru/global/security/jwt/JwtProvider.java
@@ -19,24 +19,16 @@ public class JwtProvider {
     private String secret;
 
     public String createAccessToken(UUID userId) {
-        return createToken(userId, "Access", 24 * 60L); // 1시간 // 테스트 환경에선 1일로
+        return createToken(userId, "Access", 60 * 60L); // 1시간 (3600초)
     }
 
-    public String createRefreshToken(UUID userId) {
-        return createToken(userId, "Refresh",  14 * 24 * 60L); // 14일
-    }
-
-    public String createToken(UUID userId, String type, Long expireTimeMs) {
-        try {
-            return Jwts.builder()
-                    .signWith(SignatureAlgorithm.HS512, secret)
-                    .setHeaderParam(Header.JWT_TYPE, type)
-                    .setId(userId.toString())
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs * 1000))
-                    .compact();
-        } catch (JwtException e) {
-            throw new RuntimeException();
-        }
+    private String createToken(UUID userId, String type, Long expireTimeSec) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .setHeaderParam(Header.JWT_TYPE, type)
+                .setId(userId.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expireTimeSec * 1000))
+                .compact();
     }
 }
